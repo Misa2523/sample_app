b421dee9edfac83b7ae1418bf787342f515e59c4
EC2 Default User <ec2-user@ip-172-31-19-167.ap-northeast-1.compute.internal>
2024-07-10 06:06:53 +0000

    [Finish] 3-3

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,8 +1,27 @@
 class ListsController < ApplicationController
-  def new
+#コントローラ：コントローラ内のアクション（メソッド）は、
+  #ブラウザに返すビューをviewsフォルダの中から
+  #見つけ出す役割を担っている。
+
+  #newアクションで投稿フォームのあるページを表示
+  def new #「new.html.erbとリンクしてる」
+    # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成
+    @list = List.new
   end
 
-  def index
+  #createアクションで投稿を保存する
+  def create
+    # 1.&2. データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)  #アクション内でのみ利用するためローカル変数listを使用
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # 4.トップ画面へリダイレクト
+    redirect_to '/top'
+  end
+
+  #indexアクションで一覧画面を表示
+  def index #「index.html.erbとリンクしてる」
+    @lists = List.all   #投稿したList全て表示させるため全てのデータを取得（allはメソッドの一種）
   end
 
   def show
@@ -10,4 +29,15 @@ class ListsController < ApplicationController
 
   def edit
   end
+
+  private   #一種の境界線（ここから下はこのcontrollerの中でしか呼び出せない）
+  # ストロングパラメータ（マスアサインメント脆弱性を防ぐ仕組み）
+  def list_params   #モデル名_params
+    params.require(:list).permit(:title, :body)  #保存するカラムを限定
+  end
 end
+
+
+# @～～：インスタンス変数
+# ↑ Controllerのアクションでインスタンス変数を指定すると、
+# viewファイル上でインスタンス変数に格納された情報を表示できる
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
+#ルーティング：送信されたURLに対して
+  #「どのコントローラの、どのアクション」で処理するか
+  #を決める「対応表」
+
 Rails.application.routes.draw do
   get 'lists/new'
-  get 'lists/index'
-  get 'lists/show'
+  get 'top' => 'homes#top'
+  #コントローラにフォームのデータを送信するときはpostを使用
+  post 'lists' => 'lists#create'    #'コントローラ名#アクション名'
+  get 'lists' => 'lists#index'
+  get 'lists/:id' => 'lists#show'   #.../lists/1 や .../lists/3 に該当
   get 'lists/edit'
-  get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
+# get：データを取得するためのHTTPメソッド
+  #（例）aタグなどで、画面遷移の際に新しいページを取得する場合
+# post：データを新規保存するためのメソッド
+  #（例）フォームなどの投稿をする場合
